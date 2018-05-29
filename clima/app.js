57eb65c7cc8c19c51a46c882e501360c85d5347f
Alan Antonio Ramos Herrera <alanrahe06@gmail.com>
2018-05-29 09:09:45 -0500

    Clima, carpeta zombies con dependencias

---
+++ clima/app.js
@@ -0,0 +1,44 @@
+var path = require("path");
+var express = require("express");
+var zipdb = require("zippity-do-dah");
+var ForecastIo = require("forecastio");
+
+var app = express();
+
+//Aquí va la llave
+var weather = new ForecastIo("902175731b9dcc811646f73a876d08fe");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("views",path.resolve(__dirname,"views"));
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.get(/^\/(\d{5})$/, function(req,res,next){
+    var zipcode = req.params[0];
+    var location = zipdb.zipcode(zipcode);
+    if(!location.zipcode){
+        next();
+        return;
+    }
+
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+
+    weather.forecast(latitude,longitude,function(err,data){
+        if(err){
+            next();
+            return;
+        }
+        res.json({
+            zipcode: zipcode,
+            temperature: data.currently.temperature
+        });
+    });
+});
+
+app.use(function(req,res){
+    res.status(404).render("404");
+});
+app.listen(3000);
